6bf0acf524b74e4602fc761f393ba0dab02fa21f
Stefaans <stefandprins@gmail.com>
2023-10-23 15:42:24 +0200

    Second commit
    Removed unused files and updated the project with new files.

--- test-react/src/App.js
+++ test-react/src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from "./Navbar";
+import Home from "./pages/Home";
+import Pricing from "./pages/Pricing";
+import About from "./pages/About";
 
 function App() {
+  let Component
+  switch (window.location.pathname) {
+    case "/":
+      Component = Home;
+      break;
+    case "/pricing":
+      Component = Pricing;
+      break;
+    case "/about":
+      Component = About;
+      break;
+    default:
+      break;
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React Fast!
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <Component />
+    </>
   );
 }
 
