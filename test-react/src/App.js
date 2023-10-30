b877dd25c923a73576472acf6749d3169d3e5f5c
Stefaans <stefandprins@gmail.com>
2023-10-30 21:13:47 +0200

    Third commit
    Create the react navigation bar

--- test-react/src/App.js
+++ test-react/src/App.js
@@ -2,28 +2,21 @@ import Navbar from "./Navbar";
 import Home from "./pages/Home";
 import Pricing from "./pages/Pricing";
 import About from "./pages/About";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
-  let Component
-  switch (window.location.pathname) {
-    case "/":
-      Component = Home;
-      break;
-    case "/pricing":
-      Component = Pricing;
-      break;
-    case "/about":
-      Component = About;
-      break;
-    default:
-      break;
-  }
   return (
     <>
       <Navbar />
-      <Component />
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<Home />} />  
+          <Route path="/pricing" element={<Pricing />} /> 
+          <Route path="/about" element={<About />} /> 
+        </Routes>  
+      </div> 
     </>
-  );
+  )
 }
 
 export default App;
--- test-react/src/Navbar.js
+++ test-react/src/Navbar.js
@@ -1,14 +1,22 @@
+import { Link, useMatch, useResolvedPath } from "react-router-dom"
+
 export default function Navbar() {
     return <nav className="nav">
-        <a href="/" className="site-title">Site Name</a>
+        <Link to="/" className="site-title">Site Name</Link>
         <ul>
-            <li className="active">
-                <a href="/pricing">Pricing</a>
-                
-            </li>
-            <li>
-                <a href="/about">About</a>
-            </li>
+            <CustomLink to="/pricing">Pricing</CustomLink>
+            <CustomLink to="/about">About</CustomLink>
         </ul>
     </nav>
+}
+
+function CustomLink({ to, children, ...props }){
+    const resolvedPath = useResolvedPath(to)
+    const isActive = useMatch({ path: resolvedPath.pathname, end: true })
+
+    return (
+        <li className={isActive ? "active" : ""}>
+            <Link to={to} {...props}>{children}</Link>
+        </li>
+    )
 }
